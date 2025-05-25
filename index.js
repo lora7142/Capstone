1a2eb226739d2f7133b35dba7567535f75a995ca
Lora Paro <loraparo@gmail.com>
2025-05-24 20:32:47 -0500

    Capstone Index, About, Contact v1

---
+++ index.js
@@ -0,0 +1,15 @@
+function goToAddPage() {
+  window.location.href = "Add.html";
+}
+function goToUpdateDeletePage() {
+  window.location.href = "UpdateDelete.html";
+}
+function goToReportPage() {
+  window.location.href = "Report.html";
+}
+function goToAboutPage() {
+  window.location.href = "About.html";
+}
+function goToContactPage() {
+  window.location.href = "Contact.html";
+}
