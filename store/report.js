e5cedb3525d9f7a7300f68702ec99114d73f5582
Lora Paro <loraparo@gmail.com>
2025-06-12 15:37:33 -0500

    added weather API

--- store/report.js
+++ store/report.js
@@ -1,4 +1,5 @@
 export default {
   header: "Report", // The text that shows on the view
-  view: "report" // This should match the view file name
+  view: "report", // This should match the view file name
+  reports: [] // need to declare an array to put the data coming in from the reports DB and the API
 };
--- views/home.js
+++ views/home.js
@@ -1,17 +1,23 @@
 import html from "html-literal";
-export default () => html`
+
+export default state => html`
   <section id="home">
     <h2>Weather API goes here</h2>
+    <h3>
+      The weather in ${state.weather.city} is ${state.weather.description}.
+      Temperature is ${state.weather.temp}&deg;F, and it feels like
+      ${state.weather.feelsLike}&deg;F.
+    </h3>
     <h4 class="indexH4">Today looks like a great day to be outside!</h4>
     <h3 class="indexH3">What would you like to do?</h3>
     <table class="tableButtons">
       <tr class="flex-container">
         <!-- need to figure out the code for buttons to go to different sections -->
-        <td><button onclick="goToAddPage()">Add</button></td>
-        <td><button onclick="goToUpdateDeletePage()">Update/Delete</button></td>
-        <td><button onclick="goToReportPage()">Report/Search</button></td>
-        <td><button onclick="goToAboutPage()">About</button></td>
-        <td><button onclick="goToContactPage()">Contact</button></td>
+        <td><button>Add</button></td>
+        <td><button>Update/Delete</button></td>
+        <td><button>Report/Search</button></td>
+        <td><button>About</button></td>
+        <td><button>Contact</button></td>
       </tr>
     </table>
   </section>
